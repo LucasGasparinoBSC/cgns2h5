0ad5719b5a0318aa6605fa903ed12e314d513c96
Lucas Gasparino <lucas.gasparino@bsc.es>
2024-04-12 17:59:13 +0200

    Baase element info.

--- src/cgns2h5/cgnsElemInfo.cxx
+++ src/cgns2h5/cgnsElemInfo.cxx
@@ -1,5 +1,38 @@
 #include "cgnsElemInfo.h"
 
+int* genQuadEdges()
+{
+    static int quadEdges[4][2] = {
+        {0, 1},
+        {1, 2},
+        {2, 3},
+        {3, 0}
+    };
+
+    return *quadEdges;
+}
+
+int* genHexaEdges()
+{
+    static int hexaEdges[12][2] = {
+        {0, 1},
+        {1, 2},
+        {2, 3},
+        {3, 0},
+        {4, 5},
+        {5, 6},
+        {6, 7},
+        {7, 4},
+        {0, 4},
+        {1, 5},
+        {2, 6},
+        {3, 7}
+    };
+
+    return *hexaEdges;
+}
+
+
 /**
  * @brief Get information on a specific CGNS element type
  * 
--- src/cgns2h5/inc/cgnsElemInfo.h
+++ src/cgns2h5/inc/cgnsElemInfo.h
@@ -7,6 +7,9 @@
 #include <mpi.h>
 #include <pcgnslib.h>
 
+int* genQuadEdges();
+int* genHexaEdges();
+int* genHexaFaces();
 void getElemInfo( const CGNS_ENUMT(ElementType_t) &eltype, int &numElemNodes, int &elemOrder, int &elemDim );
 
 #endif // !CGNS_ELEM_INFO_H_
